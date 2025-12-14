56453a712eae9f298d8aa73f57f6bd9f5cafc0a4
Saksham-Bansal-3110 <sakshambansal3110@gmail.com>
2025-12-14 16:11:29 +0000

    added reading the files as string for converting them to prompts

---
+++ functions/get_file_content.py
@@ -0,0 +1,35 @@
+import os
+from config import MAX_FILE_CHARS
+
+def get_file_content(working_dir, file_path):
+    try: 
+        #Resolve absolute working directory
+        working_dir_abs = os.path.abspath(working_dir)
+        
+        #Build and normalize target file path
+        target_path = os.path.normpath(os.path.join(working_dir_abs,file_path))
+        
+        #Ensure target path is within working directory
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_path])
+        if not valid_target_dir == working_dir_abs:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        
+        #Ensure target path is a regular file
+        if not os.path.isfile(target_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        
+        #Read File contents
+        with open(target_path, "r", encoding="utf-8") as f:
+            content = f.read()
+            
+        #Turnacate if necessary
+        if len(content) > MAX_FILE_CHARS:
+            content = (
+                content[:MAX_FILE_CHARS]
+                + f'\n[...File "{file_path}" turnates at {MAX_FILE_CHARS} characters]'
+            )
+            
+        return content
+        
+    except Exception as e:
+        return f"Error: {e}"
---
+++ test_get_file_content.py
@@ -0,0 +1,38 @@
+from functions.get_file_content import get_file_content
+from config import MAX_FILE_CHARS
+
+def run_test():
+    result = get_file_content("calculator","lorem.txt")
+    
+    #Ensure result is a string
+    if not isinstance(result,str):
+        print("Error: get_file_content did not return a string")
+        return
+    
+    #Check truncation
+    if len(result) <= MAX_FILE_CHARS:
+        print("Error: file was not truncated as expected")
+        return
+    
+    truncation_message = f'[...File "lorem.txt" turnates at {MAX_FILE_CHARS} characters]'
+    
+    if not result.endswith(truncation_message):
+        print("Error: truncation message missing or incorrect")
+        return
+    
+    print("Truncation test passed")
+    print(f"Returned length: {len(result)}")
+    print(f"Truncation message found at end")
+    
+    result = get_file_content("calculator","main.py")
+    print(result)
+    result = get_file_content("calculator","pkg/calculator.py")
+    print(result)
+    result = get_file_content("calculator","/bin/cat")
+    print(result)
+    result = get_file_content("calculator","pkg/does_not_exist.py")
+    print(result)
+    
+    
+if __name__ == "__main__":
+    run_test()    
